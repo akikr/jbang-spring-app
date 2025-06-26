1c1ae044a4a6fa6a88bbcf610a4894140e975c26
ankit <akikr@duck.com>
2025-06-26 19:38:06 +0530

    Modify: The AppScript.java file
    Add: A AppContoller with a simple '/hello' endpoint and the spring-boot-actuator dependency
    Add: A application.yml file

--- AppScript.java
+++ AppScript.java
@@ -4,16 +4,42 @@
 //JAVA_OPTIONS -Dserver.port=8081
 
 //DEPS org.springframework.boot:spring-boot-starter-web:3.2.5
+//DEPS org.springframework.boot:spring-boot-starter-actuator:3.2.5
 
 package com.example.jbang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class AppScript {
 
     public static void main(String... args) {
         SpringApplication.run(AppScript.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+class AppController {
+
+    private static final Logger log = LoggerFactory.getLogger(AppController.class);
+
+    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("Invoked sayHello method");
+        log.debug("Invoked sayHello method for name: {}", name);
+        return ResponseEntity.ok()
+                .body(Map.of("message", "Hello, " + name + "!"));
     }
 }
