60d99a0fe329e693f17603ae59b4435d639e84f2
ankit <akikr@duck.com>
2025-07-05 23:57:18 +0530

    Add: A ChatController to test the spring-ai chat-client
    Add the spring-ai deps and spring-ai config
    Add a command to run a LLM model locally using ollama in docker

--- AppScript.java
+++ AppScript.java
@@ -3,23 +3,31 @@
 //JAVA 21
 //JAVA_OPTIONS -Dserver.port=8081
 
-//DEPS org.springframework.boot:spring-boot-starter-web:3.2.5
-//DEPS org.springframework.boot:spring-boot-starter-actuator:3.2.5
+//DEPS org.springframework.boot:spring-boot-starter-web:3.3.0
+//DEPS org.springframework.boot:spring-boot-starter-actuator:3.3.0
+//DEPS org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-M6
 
 package com.example.jbang;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 @SpringBootApplication
 public class AppScript {
@@ -41,5 +49,43 @@ class AppController {
         log.debug("Invoked sayHello method for name: {}", name);
         return ResponseEntity.ok()
                 .body(Map.of("message", "Hello, " + name + "!"));
+    }
+}
+
+@RestController
+@RequestMapping(path = "/ai")
+class ChatController {
+
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+    private static final String SYSTEM_PROMPT = """
+            Answer the question in plain text in one sentence.
+            Answer the question in a concise manner.
+            """;
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        InMemoryChatMemory memory = new InMemoryChatMemory();
+        this.chatClient = builder
+                .defaultAdvisors(new MessageChatMemoryAdvisor(memory))
+                .build();
+    }
+
+    @PostMapping(value = "/chat", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> chat(@RequestBody String prompt) {
+        try {
+            log.info("Invoked chat method with prompt: {}", prompt);
+            String content = chatClient.prompt()
+                    .system(SYSTEM_PROMPT)
+                    .user(prompt)
+                    .call()
+                    .content();
+
+            return ResponseEntity.ok()
+                    .body((Map.of("response", requireNonNull(content, "Response content is NULL"))));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "Some error occurred , due to: " + e.getMessage()));
+        }
     }
 }
