338c1860fe04597c8ee53875f1caf43fb4203478
ankit <akikr@duck.com>
2025-09-12 00:31:59 +0530

    Add: A Yugabyte POC with yugabyte DB setup in docker-compose

--- AppScript.java
+++ AppScript.java
@@ -4,30 +4,45 @@
 //JAVA_OPTIONS -Dserver.port=8081
 
 //DEPS org.springframework.boot:spring-boot-starter-web:3.3.0
+//DEPS org.springframework.boot:spring-boot-starter-data-jpa:3.3.0
+//DEPS org.springframework.boot:spring-boot-starter-validation:3.3.0
+//DEPS org.postgresql:postgresql:42.6.0
 //DEPS org.springframework.boot:spring-boot-starter-actuator:3.3.0
-//DEPS org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-M6
+//DEPS org.apache.commons:commons-text:1.14.0
+//DEPS org.projectlombok:lombok:1.18.40
 
 package com.example.jbang;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
-
-import static java.util.Objects.requireNonNull;
 
 @SpringBootApplication
 public class AppScript {
@@ -43,49 +58,68 @@ class AppController {
 
     private static final Logger log = LoggerFactory.getLogger(AppController.class);
 
+    private final AppService appService;
+
+    AppController(AppService appService) {
+        this.appService = appService;
+    }
+
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
         log.info("Invoked sayHello method");
         log.debug("Invoked sayHello method for name: {}", name);
+        String formattedName = StringEscapeUtils.builder(StringEscapeUtils.ESCAPE_HTML4).escape(name).toString();
         return ResponseEntity.ok()
-                .body(Map.of("message", "Hello, " + name + "!"));
+                .body(Map.of("message", "Hello, %s!".formatted(formattedName)));
+    }
+
+    @GetMapping(value = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getData() {
+        return appService.getData();
     }
 }
 
-@RestController
-@RequestMapping(path = "/ai")
-class ChatController {
+@Service
+class AppService {
+    private final AppRepository appRepository;
 
-    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
-    private static final String SYSTEM_PROMPT = """
-            Answer the question in plain text in one sentence.
-            Answer the question in a concise manner.
-            """;
-
-    private final ChatClient chatClient;
-
-    public ChatController(ChatClient.Builder builder) {
-        InMemoryChatMemory memory = new InMemoryChatMemory();
-        this.chatClient = builder
-                .defaultAdvisors(new MessageChatMemoryAdvisor(memory))
-                .build();
+    AppService(AppRepository appRepository) {
+        this.appRepository = appRepository;
     }
 
-    @PostMapping(value = "/chat", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> chat(@RequestBody String prompt) {
-        try {
-            log.info("Invoked chat method with prompt: {}", prompt);
-            String content = chatClient.prompt()
-                    .system(SYSTEM_PROMPT)
-                    .user(prompt)
-                    .call()
-                    .content();
-
-            return ResponseEntity.ok()
-                    .body((Map.of("response", requireNonNull(content, "Response content is NULL"))));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest()
-                    .body(Map.of("error", "Some error occurred , due to: " + e.getMessage()));
-        }
+    public ResponseEntity<?> getData() {
+        List<Bookmark> bookmarks = appRepository.findAll();
+        return ResponseEntity.ok().body(bookmarks);
     }
+}
+
+@Repository
+interface AppRepository extends PagingAndSortingRepository<Bookmark, Long> {
+    List<Bookmark> findAll();
+}
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "bookmarks")
+class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Title cannot be NULL")
+    @NotBlank(message = "Title cannot be Blank")
+    @Column
+    private String title;
+
+    @NotNull(message = "URL cannot be NULL")
+    @NotBlank(message = "URL cannot be Blank")
+    @Column
+    private String url;
+
+    @Column
+    private LocalDateTime createdAt;
 }
